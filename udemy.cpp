4656af7efe575a5b626687093b3bee276d1ef432
Brunoespina <brunoespina05@gmail.com>
2023-06-12 18:41:53 -0300

    first commit

---
+++ udemy.cpp
@@ -0,0 +1,14 @@
+use namespace std;
+int main (){
+    int a, b;
+    a=5;
+    b=1;
+    int value = 0;
+    while (a > b){
+    b++;
+    value++;
+
+}
+count << a << " - " << b << "=" << value;
+}
+        
